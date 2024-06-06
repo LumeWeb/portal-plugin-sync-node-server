8a4cc860fdc9a3caedda69f0eb2d4d9f4b17f718
Derrick Hammer <derrick@derrickhammer.com>
2024-06-05 21:01:22 -0400

    refactor: manually keep a copy of grpc_stdio.proto and update build script

--- node/build.sh
+++ node/build.sh
@@ -11,23 +11,8 @@ cp ./node_modules/quickbit-native/prebuilds/linux-x64/quickbit-native.node app/a
 
 mkdir -p src/generated
 ./node_modules/protobufjs-cli/bin/pbjs -t json ../proto/protocol.proto > src/generated/protobuf.json
+./node_modules/protobufjs-cli/bin/pbjs -t json ../grpc/grpc_stdio.proto > src/generated/grpc_stdio.json
 
 
-# Check if vendor exists
-if [ -d "../../vendor/github.com/hashicorp" ]; then
-    proto_file="$(readlink -f ../../vendor)/github.com/hashicorp/go-plugin/internal/plugin/grpc_stdio.proto"
-# Check if GOPATH is set
-elif [ -d "$GOPATH/pkg/mod" ]; then
-    proto_file=$(find "$GOPATH/pkg/mod/github.com/hashicorp" -name grpc_stdio.proto -print -quit)
-# Check if $HOME/go/pkg/mod exists
-elif [ -d "$HOME/go/pkg/mod/github.com/hashicorp" ]; then
-    proto_file="$(find "$HOME/go/pkg/mod/github.com/hashicorp" -name grpc_stdio.proto -print -quit)"
-else
-    echo "Error: Could not find the parent directory of github.com/hashicorp"
-    exit 1
-fi
-
-# Run protobufjs-cli command
-./node_modules/protobufjs-cli/bin/pbjs -t json "$proto_file" > src/generated/grpc_stdio.json
 ./node_modules/.bin/rollup -c rollup.config.js --silent
-zip -r bundle.zip app
+zip -r ../bundle.zip app
